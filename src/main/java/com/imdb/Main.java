02ec822b3a31ce723053f37a6205d97cdb21f031
Elissa Tavares <elissasousx@alu.ufc.br>
2024-02-16 15:07:19 -0300

    final

--- src/main/java/com/imdb/Main.java
+++ src/main/java/com/imdb/Main.java
@@ -5,15 +5,15 @@ import com.imdb.infra.scanner.Input;
 
 public class Main {
     public static void main(String[] args) {
-//        Input input = new Input();
-//        int option;
-//        do {
-//            option = input.option();
-//            switch (option) {
-//                case 1 ->
-//                case 2 ->
-//                case 3 ->
-//            }
-//        } while (option != 5);
+        Input input = new Input();
+        int option;
+        do {
+            option = input.option();
+            switch (option) {
+                case 1 ->
+                case 2 ->
+                case 3 ->
+            }
+        } while (option != 5);
     }
 }
